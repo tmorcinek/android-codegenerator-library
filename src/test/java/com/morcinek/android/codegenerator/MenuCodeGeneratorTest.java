df95f02a33adaf6e7caa14cef301bb49039ff3dd
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2014-09-17 22:54:49 +0200

    Adding test: MenuCodeGeneratorTest, which tests resource extraction with code generation for menus.

---
+++ src/test/java/com/morcinek/android/codegenerator/MenuCodeGeneratorTest.java
@@ -0,0 +1,66 @@
+package com.morcinek.android.codegenerator;
+
+import com.morcinek.android.codegenerator.codegeneration.TemplateCodeGenerator;
+import com.morcinek.android.codegenerator.codegeneration.providers.factories.AdapterResourceProvidersFactory;
+import com.morcinek.android.codegenerator.codegeneration.providers.factories.MenuResourceProvidersFactory;
+import com.morcinek.android.codegenerator.codegeneration.templates.ResourceTemplatesProvider;
+import com.morcinek.android.codegenerator.codegeneration.templates.TemplatesProvider;
+import com.morcinek.android.codegenerator.extractor.XMLResourceExtractor;
+import com.morcinek.android.codegenerator.extractor.string.FileNameExtractor;
+import com.morcinek.android.codegenerator.util.InputStreamProvider;
+import org.fest.assertions.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+import java.io.IOException;
+
+public class MenuCodeGeneratorTest {
+
+    private CodeGenerator codeGenerator;
+
+    private InputStreamProvider inputStreamProvider = new InputStreamProvider();
+
+    private TemplatesProvider templatesProvider = new ResourceTemplatesProvider();
+
+    @Before
+    public void setUp() throws Exception {
+        codeGenerator = new CodeGenerator(XMLResourceExtractor.createResourceExtractor(), new FileNameExtractor(), new TemplateCodeGenerator("Menu_template", new MenuResourceProvidersFactory(), new ResourceTemplatesProvider()));
+    }
+
+    @Test
+    public void produceCodeCreateGameTest() throws Exception {
+        // given
+        // when
+        String producedCode = produceCodeFromFilePath("menus/create_game.xml");
+
+        // then
+        Assertions.assertThat(producedCode).isNotNull().isEqualTo(templatesProvider.provideTemplateForName("results/menus/CreateGame.java"));
+    }
+
+    @Test
+    public void produceCodeGamesTest() throws Exception {
+        // given
+        // when
+        String producedCode = produceCodeFromFilePath("menus/games.xml");
+
+        // then
+        Assertions.assertThat(producedCode).isNotNull().isEqualTo(templatesProvider.provideTemplateForName("results/menus/Games.java"));
+    }
+
+    @Test
+    public void produceCodeGameSessionTest() throws Exception {
+        // given
+        // when
+        String producedCode = produceCodeFromFilePath("menus/game_session.xml");
+
+        // then
+        Assertions.assertThat(producedCode).isNotNull().isEqualTo(templatesProvider.provideTemplateForName("results/menus/GameSession.java"));
+    }
+
+    private String produceCodeFromFilePath(String filePath) throws ParserConfigurationException, SAXException, XPathExpressionException, IOException {
+        return codeGenerator.produceCode(inputStreamProvider.getStreamFromResource(filePath), filePath);
+    }
+}
