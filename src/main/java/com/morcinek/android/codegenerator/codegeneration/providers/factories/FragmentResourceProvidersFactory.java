f497c2c10a77d0a9bb04427f104da7b7b1716ad9
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2014-09-19 19:14:48 +0200

    Work in progress on Generating code for fragment. Adding Fragment_template, changing related templates: Assignment_Button_template, Getter_Case_template. Adding tests - FragmentCodeGeneratorTest.

---
+++ src/main/java/com/morcinek/android/codegenerator/codegeneration/providers/factories/FragmentResourceProvidersFactory.java
@@ -0,0 +1,29 @@
+package com.morcinek.android.codegenerator.codegeneration.providers.factories;
+
+import com.google.common.collect.Lists;
+import com.morcinek.android.codegenerator.codegeneration.providers.ResourceProvider;
+import com.morcinek.android.codegenerator.codegeneration.providers.ResourceProvidersFactory;
+import com.morcinek.android.codegenerator.codegeneration.providers.resources.ButtonProvider;
+import com.morcinek.android.codegenerator.codegeneration.providers.resources.DefaultProvider;
+import com.morcinek.android.codegenerator.codegeneration.providers.resources.GetterProvider;
+import com.morcinek.android.codegenerator.extractor.model.Resource;
+
+/**
+ * Copyright 2014 Tomasz Morcinek. All rights reserved.
+ */
+public class FragmentResourceProvidersFactory implements ResourceProvidersFactory {
+
+    @Override
+    public ResourceProvider createResourceProvider(Resource resource) {
+        if (isApplicable(resource, "Button", "ImageButton")) {
+            return new ButtonProvider(resource);
+        } else if (isApplicable(resource, "CheckBox", "EditText", "View")) {
+            return new GetterProvider(resource);
+        }
+        return new DefaultProvider(resource);
+    }
+
+    private boolean isApplicable(Resource resource, String... resourcesNames) {
+        return Lists.newArrayList(resourcesNames).contains(resource.getResourceType().getFullName());
+    }
+}
---
+++ src/test/resources/results/fragments/ViewPagerFragment.java
@@ -0,0 +1,28 @@
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+
+
+public class ViewPagerFragment extends Fragment  {
+
+    private ViewPager pager;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.view_pager, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        pager = (ViewPager) view.findViewById(R.id.pager);
+
+    }
+
+
+}
